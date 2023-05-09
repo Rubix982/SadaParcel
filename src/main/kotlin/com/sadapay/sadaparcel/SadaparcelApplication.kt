24ccfa5ccea870e6435b067188f9f9e205eb2c8e
Saif Ul Islam <saif.islam@astera.com>
2023-05-10 02:25:17 +0500

    🎉 init(SadaParcel): Init commit for project
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

---
+++ src/main/kotlin/com/sadapay/sadaparcel/SadaparcelApplication.kt
@@ -0,0 +1,11 @@
+package com.sadapay.sadaparcel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SadaparcelApplication
+
+fun main(args: Array<String>) {
+	runApplication<SadaparcelApplication>(*args)
+}

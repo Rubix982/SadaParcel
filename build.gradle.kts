47674b47cc135672e2c0d9df8524bb1f87746c03
Saif Ul Islam <saif.islam@astera.com>
2023-05-12 02:51:18 +0500

    ➕ dependency(build.gradle.kts): Add dependency for JPA/H2
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,13 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
 	implementation("io.springfox:springfox-swagger2:3.0.0")
 	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.h2database:h2:2.1.214")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mockito:mockito-core:2.21.0")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")

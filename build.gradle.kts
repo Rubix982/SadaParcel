701ffc9df3c4613128808a48ad61d38ce8b76799
Saif Ul Islam <saif.islam@astera.com>
2023-05-14 03:20:20 +0500

    ➕ dependency(builds.gradle): Forgot to add `commons-io` for the HttpLoggingFilter
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 }
 
 dependencies {
+	implementation("commons-io:commons-io:2.5")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-log4j2")

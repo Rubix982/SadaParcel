4f8138131688fa89b252e5c820d8fa9b057cc442
Saif Ul Islam <saif.islam@astera.com>
2023-05-14 03:25:11 +0500

    🔧 config(SadaParcelSecurityConfiguration): Use `org.springframework.security` to allow all HTTP requests to any endpoint
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

---
+++ src/main/kotlin/com/sadapay/sadaparcel/config/SadaParcelSecurityConfiguration.kt
@@ -0,0 +1,21 @@
+package com.sadapay.sadaparcel.config
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+class SadaParcelSecurityConfiguration {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint())
+            .authorizeHttpRequests { requests -> requests.anyRequest().permitAll() }
+        return http.build()
+    }
+}
+

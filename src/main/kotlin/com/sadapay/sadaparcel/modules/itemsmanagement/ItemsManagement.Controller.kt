49b93c48422eedce8331930a4a0acf391837170c
Saif Ul Islam <saif.islam@astera.com>
2023-05-14 02:34:37 +0500

    🚧 WIP(ItemsManagement.Controller): Fix for controller returning 405 HTTP under way
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Controller.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Controller.kt
@@ -1,19 +1,62 @@
 package com.sadapay.sadaparcel.modules.itemsmanagement
 
 import com.sadapay.sadaparcel.modules.models.entities.Item
+import com.sadapay.sadaparcel.modules.models.entities.Line
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("items-management")
 class ItemsManagementController {
-    @GetMapping(produces = ["application/json"])
+
+    @GetMapping("/", produces = ["application/json"])
+    @RequestMapping(value = ["/"], method = [RequestMethod.GET])
     @ResponseBody
     fun index(): Collection<Item> = listOf()
 
-    @PostMapping(produces = ["application/json"])
+    /*
+        @PostMapping("/", produces = ["application/json"])
+        @ResponseBody
+        fun add(@RequestBody lines: List<Line>): ResponseEntity<List<Line>> {
+
+            if (lines.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body(listOf())
+            }
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(lines)
+        }
+    */
+
+    @PostMapping("/", produces = ["application/json"])
+    @RequestMapping(value = ["/"], method = [RequestMethod.POST])
     @ResponseBody
-    // TODO: Add @RequestBody annotation
-    fun add(item: Item): Item = item
+    fun add(@RequestBody json: Map<String, Any>): ResponseEntity<List<Line>> {
+
+        val lines =
+            json["lines"] as? List<LinkedHashMap<String, Any>> ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(listOf())
+
+        // Convert the LinkedHashMap to Line
+        val parsedLines = lines.map {
+            val item =
+                it["item"] as? Map<String, Any> ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val id = item["id"] as? String ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val name = item["name"] as? String ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val description =
+                item["description"] as? String ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val price = item["price"] as? Double ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val cost = item["cost"] as? Double ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            val quantity = it["quantity"] as? Int ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listOf())
+            Line(null, Item(id, name, description, price, cost, null, null), quantity)
+        }
+
+        if (parsedLines.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(listOf())
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(parsedLines)
+    }
 
     @DeleteMapping(produces = ["application/json"])
     @ResponseBody

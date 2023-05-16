10dd10f4298e2b71027063200072c7fcc9abe138
Saif Ul Islam <saif.islam@astera.com>
2023-05-17 02:27:40 +0500

    🗃 database(Item): Add `data.sql` under `resources` to run on server bootstrap
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO items (id, item_id, name, description, price, cost, order_id)
+VALUES (1, '1', 'Item 1', 'Description for Item 1', 10.0, 5.0, NULL),
+       (2, '2', 'Item 2', 'Description for Item 2', 15.0, 7.5, NULL),
+       (3, '3', 'Item 3', 'Description for Item 3', 20.0, 10.0, NULL),
+       (4, '4', 'Item 4', 'Description for Item 4', 25.0, 12.5, NULL),
+       (5, '5', 'Item 5', 'Description for Item 5', 30.0, 15.0, NULL),
+       (6, '6', 'Item 6', 'Description for Item 6', 35.0, 17.5, NULL),
+       (7, '7', 'Item 7', 'Description for Item 7', 40.0, 20.0, NULL),
+       (8, '8', 'Item 8', 'Description for Item 8', 45.0, 22.5, NULL),
+       (9, '9', 'Item 9', 'Description for Item 9', 50.0, 25.0, NULL),
+       (10, '10', 'Item 10', 'Description for Item 10', 55.0, 27.5, NULL);

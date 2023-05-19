1bfe98616c089277649714d6e39753a0cecfc315
Saif Ul Islam <saif.islam@astera.com>
2023-05-20 03:13:38 +0500

    ♻ refactor(Item.Controller): Use repository instead of service for now

--- src/main/kotlin/com/sadapay/sadaparcel/modules/item/Item.Controller.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/item/Item.Controller.kt
@@ -1,15 +1,25 @@
 package com.sadapay.sadaparcel.modules.item
 
+import com.sadapay.sadaparcel.modules.models.entities.Item
+import com.sadapay.sadaparcel.modules.models.repositories.ItemRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
-class ItemsController(
-    @Autowired val itemService: ItemService
-) {
+class ItemsController @Autowired constructor(itemRepository: ItemRepository) {
+    private val itemRepository: ItemRepository
+
+    init {
+        this.itemRepository = itemRepository
+    }
+
     @GetMapping("/items", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    fun index() = itemService.findAll()
+    fun index(): MutableIterable<Item?> = itemRepository.findAll()
 }

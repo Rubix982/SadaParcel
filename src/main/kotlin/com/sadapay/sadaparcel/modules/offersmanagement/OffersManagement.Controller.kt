94bf1e46b9b23659c91aa6aa5c37ee0b82f855b1
Saif Ul Islam <saif.islam@astera.com>
2023-05-20 15:12:13 +0500

    👔 necktie(OffersManagement.Controller): Add controller mappings for GET, POST, and DELETE
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Controller.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Controller.kt
@@ -1,24 +1,59 @@
 package com.sadapay.sadaparcel.modules.offersmanagement
 
 import com.sadapay.sadaparcel.modules.models.entities.Offer
+import com.sadapay.sadaparcel.modules.offer.OfferDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
-@RequestMapping("offers-management")
 @RestController
-class OffersManagementController {
+class OffersManagementController @Autowired constructor(offersManagementService: OffersManagementService?) {
+
+    private val offersManagementService: OffersManagementService?
+
+    init {
+        this.offersManagementService = offersManagementService
+    }
+
     @GetMapping(produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    fun getOffersManagement(): List<Offer> =
-        listOf(
-            Offer("1", "offer1", "OFFER1", "1.0", 1.1, 1),
-            Offer("2", "offer2", "OFFER2", "2.0", 1.1, 1)
-        )
+    fun getOffersManagement(): MutableIterable<Offer?>? = offersManagementService?.findAll()
 
-    @PostMapping(produces = ["application/json"])
+    @PostMapping("/offers-management", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    fun addOffer(offer: Offer): Offer = offer
+    fun addOffer(@Valid @RequestBody offerDto: OfferDto): ResponseEntity<OfferDto> {
 
-    @DeleteMapping(produces = ["application/json"])
+        if (offerDto.itemId.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(offerDto)
+        }
+
+        offersManagementService?.save(offerDto)
+        return ResponseEntity.status(HttpStatus.CREATED).body(offerDto)
+    }
+
+    @DeleteMapping("/offers-management", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    fun removeOffer(offer: Offer): Offer = offer
+    fun removeOffer(offerIds: List<String>): ResponseEntity<List<String>> {
+
+        if (offerIds.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(listOf())
+        }
+
+        offersManagementService?.areAllIdsValid(offerIds)?.let { areAllIdsValid ->
+            if (!areAllIdsValid) {
+                // If offerIds has some or all invalid Ids, return a 409 Conflict
+                // https://stackoverflow.com/questions/25122472/rest-http-status-code-if-delete-impossible
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(listOf())
+            }
+        }
+
+        offersManagementService?.deleteOffers(offerIds)
+
+        return ResponseEntity.status(HttpStatus.OK).body(offerIds)
+    }
 }

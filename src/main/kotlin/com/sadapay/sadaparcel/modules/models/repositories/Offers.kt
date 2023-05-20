ba9e5c1771b72ad76964fdcdc4865a881b0bed95
Saif Ul Islam <saif.islam@astera.com>
2023-05-20 15:16:07 +0500

    👔 necktie(Offers, OffersManagement): Connect service and repository with methods. Remove JPA and PagingAndSortingRepository.
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/models/repositories/Offers.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/models/repositories/Offers.kt
@@ -1,11 +1,21 @@
 package com.sadapay.sadaparcel.modules.models.repositories
 
 import com.sadapay.sadaparcel.modules.models.entities.Offer
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface OfferRepository : CrudRepository<Offer?, Long?>, JpaRepository<Offer?, Long?>,
-    PagingAndSortingRepository<Offer?, Long?>
+interface OfferRepository : CrudRepository<Offer?, Long?> {
+
+    fun findByItemId(itemId: String): Optional<Offer>
+
+    @Query("SELECT COUNT(*) FROM Offer offer WHERE offer.itemId IN :offerIds")
+    fun countByOfferIds(offerIds: List<String>): Long
+
+    @Modifying
+    @Query("DELETE FROM Offer WHERE itemId IN :offerIds")
+    fun deleteByOfferIds(offerIds: List<String>)
+}
--- src/main/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Service.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Service.kt
@@ -2,15 +2,42 @@ package com.sadapay.sadaparcel.modules.offersmanagement
 
 import com.sadapay.sadaparcel.modules.models.entities.Offer
 import com.sadapay.sadaparcel.modules.models.repositories.OfferRepository
+import com.sadapay.sadaparcel.modules.offer.OfferDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class OffersManagementService(
     @Autowired
     val offerRepository: OfferRepository
 ) {
-    fun list(): List<Offer?>? {
+    fun findAll(): MutableIterable<Offer?> {
         return offerRepository.findAll()
+    }
+
+    fun save(offerDto: OfferDto): Offer {
+
+        var offer: Optional<Offer> = offerRepository.findByItemId(offerDto.itemId)
+
+        if (offer.isEmpty) {
+            offer = Optional.of(Offer(offerDto))
+        } else {
+            offer.get().name = offerDto.name
+            offer.get().description = offerDto.description
+            offer.get().priceReduction = offerDto.priceReduction
+            offer.get().quantityThreshold = offerDto.quantityThreshold
+        }
+
+        return offerRepository.save(offer.get())
+    }
+
+    fun areAllIdsValid(offerIds: List<String>): Boolean {
+        val count: Long = offerRepository.countByOfferIds(offerIds)
+        return count == offerIds.size.toLong()
+    }
+
+    fun deleteOffers(offerIds: List<String>) {
+        offerRepository.deleteByOfferIds(offerIds)
     }
 }

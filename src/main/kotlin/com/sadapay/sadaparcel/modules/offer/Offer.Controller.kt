67c88a3d4968c800cd3dd824e8f9b0915f46d304
Saif Ul Islam <saif.islam@astera.com>
2023-05-20 15:09:20 +0500

    👔 necktie(Offer.Controller): Update controller configuration and mapping
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/offer/Offer.Controller.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/offer/Offer.Controller.kt
@@ -1,15 +1,23 @@
 package com.sadapay.sadaparcel.modules.offer
 
 import com.sadapay.sadaparcel.modules.models.entities.Offer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("offers")
 @RestController
-class OffersController {
-    @GetMapping(produces = ["application/json"])
+class OffersController @Autowired constructor(offerService: OfferService?) {
+    private val offerService: OfferService?
+
+    init {
+        this.offerService = offerService
+    }
+
+    @GetMapping("/offers", produces = ["application/json"])
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    fun getOffers(): List<Offer> = listOf(Offer())
+    fun getOffers(): MutableIterable<Offer?>? = offerService?.findAll()
 }

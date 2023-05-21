db80e9b85d6da8316c83fefeb15cfd26c866fa13
Saif Ul Islam <saif.islam@astera.com>
2023-05-22 02:02:42 +0500

    👔 business(Line.Service): Add logic to `save` `Line` and `Item`
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/line/Line.Service.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/line/Line.Service.kt
@@ -1,5 +1,8 @@
 package com.sadapay.sadaparcel.modules.line
 
+import com.sadapay.sadaparcel.modules.item.ItemDto
+import com.sadapay.sadaparcel.modules.itemsmanagement.ItemsManagementService
+import com.sadapay.sadaparcel.modules.models.entities.Item
 import com.sadapay.sadaparcel.modules.models.entities.Line
 import com.sadapay.sadaparcel.modules.models.repositories.LineRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,9 +11,23 @@ import org.springframework.stereotype.Service
 @Service
 class LineService(
     @Autowired
-    val lineRepository: LineRepository
+    val lineRepository: LineRepository,
+    @Autowired
+    val itemsManagementService: ItemsManagementService
 ) {
     fun list(): List<Line?>? {
         return lineRepository.findAll()
+    }
+
+    fun save(lines: LinesDto) {
+        for (line in lines.lines) {
+            val itemDto: ItemDto = line.itemDto
+            itemsManagementService.save(itemDto)
+            save(Line(Item(itemDto), line.quantity))
+        }
+    }
+
+    fun save(line: Line) {
+        lineRepository.save(line)
     }
 }

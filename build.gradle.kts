22bf19c1e2ce76cd485e786eab7539f4e3039ef2
Saif Ul Islam <saif.islam@astera.com>
2023-05-29 01:19:23 +0500

    ➕ dependency(build.gradle.kts): Add flyway dependency and conf
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,30 @@
+/*
+ * Copyright 2023, Saif Ul Islam @ SadaParcel, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.1"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("io.swagger.core.v3.swagger-gradle-plugin") version "2.2.9"
-	kotlin("jvm") version "1.5.10"
-	kotlin("plugin.spring") version "1.5.10"
+    id("org.springframework.boot") version "2.5.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.swagger.core.v3.swagger-gradle-plugin") version "2.2.9"
+    id("org.flywaydb.flyway") version "8.5.13"
+    kotlin("jvm") version "1.5.10"
+    kotlin("plugin.spring") version "1.5.10"
 }
 
 group = "com.sadapay"
@@ -13,48 +32,48 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("commons-io:commons-io:2.5")
-	implementation("javax.validation:validation-api:2.0.1.Final")
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-log4j2")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.security:spring-security-web:5.6.5")
-	implementation("org.springframework.security:spring-security-core:5.6.5")
-	implementation("org.springframework.security:spring-security-config:5.6.5")
-	implementation("io.springfox:springfox-swagger2:3.0.0")
-	implementation("io.springfox:springfox-swagger-ui:3.0.0")
-	implementation("io.springfox:springfox-boot-starter:3.0.0")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.h2database:h2:2.1.214")
-	implementation("org.apache.tomcat:tomcat-dbcp:10.1.8")
-	implementation("org.apache.commons:commons-dbcp2:2.9.0")
-	implementation("org.hibernate.validator:hibernate-validator:6.0.2.Final")
-	implementation("org.projectlombok:lombok:1.18.20")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.mockito:mockito-core:3.7.0")
-	testImplementation("junit:junit:4.13.2")
-	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-	testImplementation("io.rest-assured:spring-mock-mvc:5.3.0")
+    implementation("commons-io:commons-io:2.5")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.security:spring-security-web:5.6.5")
+    implementation("org.springframework.security:spring-security-core:5.6.5")
+    implementation("org.springframework.security:spring-security-config:5.6.5")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.h2database:h2:2.1.214")
+    implementation("org.apache.tomcat:tomcat-dbcp:10.1.8")
+    implementation("org.apache.commons:commons-dbcp2:2.9.0")
+    implementation("org.hibernate.validator:hibernate-validator:6.0.2.Final")
+    implementation("org.projectlombok:lombok:1.18.20")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mockito:mockito-core:3.7.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("io.rest-assured:spring-mock-mvc:5.3.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 configurations.all { exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging") }

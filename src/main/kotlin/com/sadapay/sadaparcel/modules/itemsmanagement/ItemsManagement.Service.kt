41c40ea3641543e2e1c08eb24dba6dc2950ee8fd
Saif Ul Islam <saif.islam@astera.com>
2023-05-28 23:11:14 +0500

    ➕ class(ItemsManagement.Service): Change logic to deal with EntityWithLogs and composer
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Service.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Service.kt
@@ -1,11 +1,15 @@
 package com.sadapay.sadaparcel.modules.itemsmanagement
 
 import com.sadapay.sadaparcel.modules.item.ItemDto
+import com.sadapay.sadaparcel.modules.models.entities.EntityWithLogs
 import com.sadapay.sadaparcel.modules.models.entities.Item
 import com.sadapay.sadaparcel.modules.models.repositories.ItemRepository
+import com.sadapay.sadaparcel.modules.transformations.TransformationMonadComposer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.io.Serializable
 import javax.transaction.Transactional
+import com.sadapay.sadaparcel.modules.itemsmanagement.ItemsManagementLogger.Companion as logger
 
 @Service
 class ItemsManagementService(
@@ -17,18 +21,27 @@ class ItemsManagementService(
     }
 
     @Transactional
-    fun save(itemDto: ItemDto): Item {
+    fun save(entity: EntityWithLogs<Serializable?>): EntityWithLogs<Serializable?> {
+        val itemDto = entity.entity as ItemDto
         val existingItem = itemRepository.findByItemId(itemDto.id).orElse(null)
         val item = existingItem?.apply {
+            logger.logApplyingChanges(entity, itemDto)
+
             name = itemDto.name
             description = itemDto.description
             price = itemDto.price
             cost = itemDto.cost
+
+            (entity.configProcessor as ItemsManagementMonadProcessor).wasAnItemUpdated = true
         }
             ?: Item(itemDto).apply {
+                logger.logNewItem(entity, itemDto)
                 id = itemRepository.count() + 1
             }
-        return itemRepository.save(item)
+
+        val savedItem: Item = itemRepository.save(item)
+        logger.logSavedItem(entity, savedItem)
+        return TransformationMonadComposer.wrapWithLogs(savedItem)
     }
 
     fun areAllIdsValid(itemIds: List<String>): Boolean {

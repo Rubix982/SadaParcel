d5994935f5cb2d7ca60dacac92426738972bed01
Saif Ul Islam <saif.islam@astera.com>
2023-05-28 23:36:11 +0500

    ✏ messages(Constants): Use a `standard` way of logging
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Constants.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/itemsmanagement/ItemsManagement.Constants.kt
@@ -1,9 +1,9 @@
 package com.sadapay.sadaparcel.modules.itemsmanagement
 
 object ItemsManagementConstants {
-    const val ITEM_SAVED = "Item saved: %s"
-    const val NEW_ITEM_CREATED = "New item with id %s created with values: %s"
+    const val ITEM_SAVED = "[SAVING]: Item saved: %s"
+    const val NEW_ITEM_CREATED = "[NEW]: New item with id %s created with values: %s"
     const val EXISTING_ITEM_APPLYING_CHANGES =
-        "Item with id %s already exists, updating it instead with new values, old values: %s, new values: %s"
+        "[EXISTING] Item with id %s already exists, updating it instead with new values, old values: %s, new values: %s"
     const val ROUTE = "/items-management"
 }
--- src/main/kotlin/com/sadapay/sadaparcel/modules/line/Line.Constants.kt
+++ src/main/kotlin/com/sadapay/sadaparcel/modules/line/Line.Constants.kt
@@ -1,5 +1,5 @@
 package com.sadapay.sadaparcel.modules.line
 
 object LineConstants {
-    const val LINE_SAVED = "Line saved: %s"
+    const val LINE_SAVED = "[SAVING]: Line saved: %s"
 }

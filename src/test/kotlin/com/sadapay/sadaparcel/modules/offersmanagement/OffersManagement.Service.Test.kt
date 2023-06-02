9f449c1272f41a9ae46410b11c90b3cd2c59f006
Saif Ul Islam <saif.islam@astera.com>
2023-06-03 01:47:35 +0500

    🧪 test(OffersManagement.Service): Fix broken test
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- src/test/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Service.Test.kt
+++ src/test/kotlin/com/sadapay/sadaparcel/modules/offersmanagement/OffersManagement.Service.Test.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2023, Saif Ul Islam @ SadaParcel, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 package com.sadapay.sadaparcel.modules.offersmanagement
 
 import org.junit.jupiter.api.AfterEach
@@ -27,11 +45,11 @@ internal class OffersManagementServiceTest(@Autowired private val offersManageme
     fun whenApplicationStarts_ThenHibernateCreatesInitialRecords() {
 
         // Given a fetched list from the ItemService
-        val items = offersManagementService.findAll()
+        val offers = offersManagementService.findAll()
 
         // When Hibernate creates initial records
 
         // Then the list is empty
-        assert(items.count() == 0)
+        assert(offers.offers.size == 0)
     }
 }
